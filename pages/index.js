c5b427408d0556526e1078fbce6cc58b892fdef5
HYAR7E <psiro.hyarte@gmail.com>
2020-09-07 18:03:33 -0500

    Google Analytics first setup

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,26 @@ const ContactInformation = createContext({
   link_whatsapp: "https://wa.me/+51xxxxxxxxx",
   link_email: "gpslatam@gmail.com",
 })
+const HTMLElementsData = createContext({
+  open_email_modal: "open-email-modal",
+  email_modal: "modal-correo",
+})
 
 export default function Home(){
   return (
     <div className={styles.container}>
       {/* This will be placed in head outisde document's body*/}
       <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C4V7WS9SFB"></script>
+        {`<script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-C4V7WS9SFB');
+        </script>`}
+
         <title>GPS Latam</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
@@ -23,6 +37,7 @@ export default function Home(){
         <link rel="stylesheet" href="/styles/landing.css" />
         {/* Acumin-pro font */}
         <link rel="stylesheet" href="https://use.typekit.net/rir3mhi.css" />
+        {/* JS */}
         <script src="/js/yt.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/emailjs-com@2.4.1/dist/email.min.js"></script>
       </Head>
@@ -39,6 +54,8 @@ export default function Home(){
 
         <Footer />
       </main>
+
+      <ModalCorreo />
     </div>
   )
 }
@@ -157,30 +174,14 @@ function Pane4({}){
   )
 }
 function Pane5({}){
-  const contexto = useContext(ContactInformation)
+  const ctx_contact = useContext(ContactInformation)
+  const ctx_html = useContext(HTMLElementsData)
 
   // Execute after content is rendered
   useEffect(() => {
     // Add functionality to hide/show contact form
-    document.getElementById('email-button').onchange = ev => {
-      document.getElementById('contact-form').style.display = ev.target.checked?"inline-block":"none"
-    }
-  }, [])
-  useEffect(() => {
-    // Initialize emailjs service
-    emailjs.init('user_PzgaERx4isvQKUCLloFHr')
-
-    // Send form as email
-    document.getElementById('c-send').onclick = ev => {
-      // Stop form submit propagation
-      ev.preventDefault()
-      // Send form content as email
-      emailjs.sendForm('test_mine_1', 'contacto_form', ev.target.parentElement)
-      .then(res => {
-        console.log("Response:", res);
-      }, er => {
-        console.log("Error:", er);
-      })
+    document.getElementById(ctx_html.open_email_modal).onclick = () => {
+      document.getElementById(ctx_html.email_modal).style.display = "inline-block"
     }
   }, [])
 
@@ -189,53 +190,38 @@ function Pane5({}){
       <div className={styles.half_1}>
         <div>
           <h1>Contáctanos</h1>
-          <span><b>Celular: </b>{contexto.contacto_celular}</span>
+          <span><b>Celular: </b>{ctx_contact.contacto_celular}</span>
           <br/>
-          <span><b>Correo electronico: </b>{contexto.contacto_email}</span>
+          <span><b>Correo electronico: </b>{ctx_contact.contacto_email}</span>
           <p>Si tienes dudas o sugerencias.</p>
-          <a target="_blank" href={contexto.link_whatsapp} className={styles.wsp}>
-            <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Fwsp-icon.png?alt=media&token=59e9e5e6-a49e-4206-8a2b-e45ff1ae58de" /><span> Escríbenos al Whatsapp</span>
+          <a data-focusable="true" target="_blank" href={ctx_contact.link_whatsapp} className={styles.wsp}>
+            <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Fwsp-icon.png?alt=media&token=59e9e5e6-a49e-4206-8a2b-e45ff1ae58de" />
+            <span> Escríbenos al Whatsapp</span>
           </a>
           <br/><br/>
 
           {/* Checkbox behavior to show contact form */}
-          <label htmlFor="email-button" className={styles.email}>
+          <a data-focusable="true" className={styles.email} id={ctx_html.open_email_modal}>
             <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Femail-icon.png?alt=media&token=f18e574f-f544-4807-a775-dde2f8060470" />
             <span> Enviar un correo electrónico</span>
-          </label>
+          </a>
         </div>
-      </div>
-      <div className={styles.half_2}>
-        <input type="checkbox" id="email-button" /> {/* Hidden element */}
-        <form id="contact-form">
-
-          <span className={styles.h1}>Enviar un correo</span>
-
-          <input type="text" name="user_name" placeholder="Nombre" />
-          <br/>
-          <input type="email" name="user_email" placeholder="Correo electronico" />
-          <br/>
-          <textarea rows="2" name="mensaje" placeholder="Contenido">
-          </textarea>
-          <br/>
-          <button type="submit" id="c-send">Enviar</button>
-        </form>
       </div>
     </section>
   )
 }
 function Footer({}){
-  const contexto = useContext(ContactInformation)
+  const ctx_contact = useContext(ContactInformation)
 
   return (
     <footer className={styles.footer}>
       <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Flogo-footer.png?alt=media&token=eed4ca0a-045d-4730-a7cc-2afc9cad704e" alt="GPS Latam logo" className={styles.logo_footer+" "+styles.item} />
       <div className={styles.contacto}>
         <div className={styles.item}>
-          <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Fphone-icon.png?alt=media&token=97dcec8a-f9ee-4efd-a9e8-683787f9235d" alt="Phone icon" /> {contexto.contacto_celular}
+          <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Fphone-icon.png?alt=media&token=97dcec8a-f9ee-4efd-a9e8-683787f9235d" alt="Phone icon" /> {ctx_contact.contacto_celular}
         </div>
         <div className={styles.item}>
-          <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Fmail-icon.png?alt=media&token=ab642a9f-61b0-4530-b604-a52eae65306d" alt="Mail icon" /> {contexto.contacto_email}
+          <img src="https://firebasestorage.googleapis.com/v0/b/hawk-peru.appspot.com/o/LandingPage_GPSLatam%2Fmail-icon.png?alt=media&token=ab642a9f-61b0-4530-b604-a52eae65306d" alt="Mail icon" /> {ctx_contact.contacto_email}
         </div>
         <div id={styles.social} className={styles.item}>
           <a target="_blank" href="https://www.facebook.com/gaming/AN7HONY96oficial">
@@ -248,5 +234,95 @@ function Footer({}){
         </div>
       </div>
     </footer>
+  )
+}
+function ModalCorreo({}){
+  const ctx_html = useContext(HTMLElementsData)
+  let modal_close_id = "modal-close"
+  let form_button_id = "c-send"
+
+  const closeModal = ev => {
+    if(ev.target.id == modal_close_id) document.getElementById(ctx_html.email_modal).style.display = "none"
+  }
+
+  useEffect(() => {
+    // Initialize emailjs service
+    emailjs.init('user_PzgaERx4isvQKUCLloFHr')
+
+    // Send form as email
+    document.getElementById(form_button_id).onclick = ev => {
+      // Stop form submit propagation
+      ev.preventDefault()
+
+      // Validate data
+      form_element = ev.target.parentElement;
+      if(
+        form_element.checkValidity() &&
+        /\S+@\S+\.\S+/.test(form_element[4].value)
+      ){
+        // Send form content as email
+        emailjs.sendForm('test_mine_1', 'contacto_form', form_element)
+        .then(res => {
+          console.log("Response:", res);
+        }, er => {
+          console.log("Error:", er);
+        })
+      }else{
+        alert("Debe llenar los campos obligatorios con datos reales")
+      }
+    }
+  }, [])
+
+  return (
+    <div id={ctx_html.email_modal}>
+      <div className={styles.modal_container} id={modal_close_id} onClick={closeModal}>
+        <div className={styles.modal}>
+          <form id={styles.formulary}>
+            <div className={styles.formulary_padding_left+" "+styles.flex_display}>
+              <div className={styles.indications}>Llene todos sus datos en los campos vacíos.</div>
+              <div className={styles.required_fields}>
+                <div className={styles.mark}>(*) </div>
+                <div className={styles.text}>Campos obligatorios</div>
+              </div>
+              <div className={styles.questions_container}>
+                <div className={styles.input_container+" "+styles.flex_display}>
+                  <div className={styles.required_mark+" "+styles.form_text}>(*)</div>
+                  <input className={styles.input+" "+styles.flex_display} name="user_name" placeholder="Nombres y Apellidos" autoCapitalize="sentences" autoComplete="on" autoCorrect="on" spellCheck="true" type="text" required />
+                </div>
+                <div className={styles.container_flex}>
+                  <div className={styles.input_container+" "+styles.flex_display}>
+                    <div className={styles.required_mark+" "+styles.form_text}>(*)</div>
+                    <input className={styles.input+" "+styles.flex_display} name="user_dni" placeholder="DNI/Pasaporte" autoCapitalize="sentences" autoComplete="on" autoCorrect="on" spellCheck="true" type="text" required />
+                  </div>
+                  <div className={styles.input_container+" "+styles.flex_display}>
+                    <div className={styles.required_mark+" "+styles.form_text}>(*)</div>
+                    <input className={styles.input+" "+styles.flex_display} name="user_cel" placeholder="Celular" autoCapitalize="sentences" autoComplete="on" autoCorrect="on" spellCheck="true" type="text" required />
+                  </div>
+                </div>
+                <div className={styles.input_container+" "+styles.flex_display}>
+                  <div className={styles.required_mark+" "+styles.form_text}>(*)</div>
+                  <input className={styles.input+" "+styles.flex_display} name="user_vehicle_type" placeholder="Tipo de vehículo/Cantidad" autoCapitalize="sentences" autoComplete="on" autoCorrect="on" spellCheck="true" type="text" required />
+                </div>
+                <div className={styles.container_flex}>
+                  <div className={styles.input_container+" "+styles.flex_display}>
+                    <div className={styles.required_mark+" "+styles.form_text}>(*)</div>
+                    <input className={styles.input+" "+styles.flex_display} name="user_email" placeholder="Correo electrónico" autoCapitalize="sentences" autoComplete="on" autoCorrect="on" spellCheck="true" type="text" required />
+                  </div>
+                  <div className={styles.send}>
+                    <div id={form_button_id} className={styles.send_button+" "+styles.form_text}>Enviar</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.input_message+" "+styles.flex_display}>
+              <div className={styles.text+" "+styles.form_text}>Envíenos su pregunta.</div>
+              <textarea className={styles.textarea+" "+styles.flex_display+" "+styles.input_container} name="message" placeholder="Escriba aquí" autoCapitalize="sentences" autoComplete="on" autoCorrect="on" rows="1" spellCheck="true">
+              </textarea>
+              <div className={styles.text_right+" "+styles.form_text}>300 caracteres</div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
